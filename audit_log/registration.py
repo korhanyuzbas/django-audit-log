c31f9b85db915de81417ebfd8cefa1193346a33d
baris bilgic <brsbilgic@gmail.com>
2016-12-27 20:05:38 +0000

    Support concrete model inheritance

--- audit_log/registration.py
+++ audit_log/registration.py
@@ -8,10 +8,15 @@ class FieldRegistry(object):
     def add_field(self, model, field):
         reg = self.__class__._registry.setdefault(self._fieldcls, {}).setdefault(model, [])
         reg.append(field)
-    
+
     def get_fields(self, model):
-        return self.__class__._registry.setdefault(self._fieldcls, {}).get(model, [])
-    
+        fields = self.__class__._registry.setdefault(self._fieldcls, {}).get(model, [])
+
+        # If the model inherits a concrete model which inherits an AuthStamp model
+        for base_model in model.__bases__:
+            fields.extend(self.__class__._registry.setdefault(self._fieldcls, {}).get(base_model, []))
+        return fields
+
     def __contains__(self, model):
-        return model in self.__class__._registry.setdefault(self._fieldcls, {})
+        return not len(set([model, model.__bases__]) & set(self.__class__._registry.setdefault(self._fieldcls, {})))
 
